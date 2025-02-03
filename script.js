552d7f6111f2352b7f5d4e074c53920e754ad3a2
flashburnn <123958002+flashburnn@users.noreply.github.com>
2025-02-04 02:38:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    document.getElementById('responseMessage').innerText = "Yay! I can't wait to spend Valentine's Day with you! 💖";
+  });
+  
+  document.getElementById('noButton').addEventListener('click', function() {
+    document.getElementById('responseMessage').innerText = "That's okay! Let me know if you change your mind. 😊";
+  });
